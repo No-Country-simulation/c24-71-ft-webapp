87bfdbd5b704612d0226cd5de8956a7fc06b22c8
Rosario Díaz <46304949+ChayChaio@users.noreply.github.com>
2025-02-19 12:16:21 -0300

    Add files via upload

---
+++ Backend/src/test/java/tech/nocountry/c24_71_ft_webapp/C2471FtWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nocountry.c24_71_ft_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2471FtWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
