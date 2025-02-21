b3ceee6d71b08232ff1ae3b0412e0eec7c765e7a
NikoDemo96 <chnicolas.aranguiz@gmail.com>
2025-02-21 12:14:23 -0400

    feat(frontend): add react + tailwind project with vite

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className ="bg-blue-400">
+          HOLA MUNDO CON TAILWIND
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
