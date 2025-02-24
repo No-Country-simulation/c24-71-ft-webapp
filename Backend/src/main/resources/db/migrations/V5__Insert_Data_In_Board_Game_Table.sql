db816a0759016e89dd7d69cfaea62973ca67342d
JissyMerlano <sicaji@gmail.com>
2025-02-24 11:58:11 -0500

    fix(database): changes enum values in V1/V5 migrations

--- Backend/src/main/resources/db/migrations/V5__Insert_Data_In_Board_Game_Table.sql
+++ Backend/src/main/resources/db/migrations/V5__Insert_Data_In_Board_Game_Table.sql
@@ -8,8 +8,8 @@ VALUES (
      3. Si toca los bordes del camino, debe reiniciar desde el último punto de control.
      4. A medida que avanza, el camino se vuelve más angosto y con más obstáculos.
      5. El tiempo de reacción y la precisión son clave para completar el nivel.',
-    'MOTOR COORDINATION',
-    'DEDUCTION GAME',
+    'MOTOR_COORDINATION',
+    'DEDUCTION_GAME',
     TRUE,
     CURRENT_TIMESTAMP
 );
@@ -22,8 +22,8 @@ INSERT INTO board_games(id_game, name, description, rules, category, type, statu
 	'Juego emparejamiento de fichas',
 	'Un juego diseñado para mejorar la memoria y la atención, donde los jugadores deben encontrar pares de fichas iguales.',
 	'1. Se muestran todas las fichas boca abajo. 2. Los jugadores deben voltear dos fichas por turno. 3. Si las fichas coinciden, se retiran del tablero. 4. Si no coinciden, se vuelven a colocar boca abajo. 5. El juego continúa hasta que todas las fichas sean emparejadas.',
-	 'MEMORY IMPROVEMENT',
-	 'MATCHING GAME',
+	 'MEMORY_IMPROVEMENT',
+	 'MATCHING_GAME',
 	 TRUE,
      CURRENT_TIMESTAMP
 );
@@ -37,8 +37,8 @@ VALUES (
      2. El jugador debe identificar todas las diferencias antes de que acabe el tiempo.
      3. Si encuentra todas las diferencias correctamente, gana puntos.
      4. Si el tiempo se agota sin encontrar todas, puede volver a intentarlo.',
-    'ATTENTION TRAINING',
-    'DEDUCTION GAME',
+    'ATTENTION_TRAINING',
+    'DEDUCTION_GAME',
     TRUE,
     CURRENT_TIMESTAMP
 );
