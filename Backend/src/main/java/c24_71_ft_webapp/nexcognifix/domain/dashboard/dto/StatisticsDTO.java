80dd1d4e8f19982b2ecd30484471ad1a7e5b540b
ChayChaio <chaychaio@gmail.com>
2025-03-05 20:23:21 -0300

    feat(dashboard) add statistics and recent sessions list

--- Backend/src/main/java/c24_71_ft_webapp/nexcognifix/domain/dashboard/dto/StatisticsDTO.java
+++ Backend/src/main/java/c24_71_ft_webapp/nexcognifix/domain/dashboard/dto/StatisticsDTO.java
@@ -1,13 +1,9 @@
 package c24_71_ft_webapp.nexcognifix.domain.dashboard.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class StatisticsDTO {
-    private Integer pendingSessionsThisMonth;
-    private Integer completedSessionsThisMonth;
-    private Integer createdSessionsThisMonth;
-    private Integer activePatientsThisMonth;
+public record StatisticsDTO(
+        Integer pendingSessionsThisMonth,
+        Integer completedSessionsThisMonth,
+        Integer createdSessionsThisMonth,
+        Integer activePatientsThisMonth
+){
 }
