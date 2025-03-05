e9de5491efe064c5f55984d874d8e29d3d82db0c
jissykakin <sicaji22@gmail.com>
2025-03-05 13:56:57 -0500

    fix(style/library): change styles in pages and install axios and reactrouter

--- Frontend/src/js/component/SideBar.jsx
+++ Frontend/src/js/component/SideBar.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { MdHome } from "react-icons/md";
 import { MdGroups } from "react-icons/md";
 import { BiSolidCastle } from "react-icons/bi";
+import { Link } from 'react-router-dom';
 
 
 const SideBar = () => {
     return (
-<div className='side-bar bg-[#4e5c82] w-[333px] h-screen'>
+<div className='side-bar bg-[#4e5c82] min-w-[333px] h-screen'>
 <div className='pages text-[#ECE5DE] text-[27px] font-semibold '>
   <ul className='flex flex-col gap-7 pl-14 pt-6'>
-    <li className='flex items-center gap-2'><MdHome /><a href=''>Home</a></li>
-    <li className='flex items-center gap-2'><MdGroups /><a href=''>Pacientes</a></li>
-    <li className='flex items-center gap-2'><BiSolidCastle /><a href=''>Juegos</a></li>
+    <li className='flex items-center gap-2'><MdHome /> <Link to="/dashboard/">Home</Link></li>
+    <li className='flex items-center gap-2'><MdGroups /><Link to="/dashboard/patients">Pacientes</Link></li>
+    <li className='flex items-center gap-2'><BiSolidCastle /><Link to="/dashboard/games">Juegos</Link></li>
   </ul>
 </div>
 </div>
--- Frontend/src/js/main.jsx
+++ Frontend/src/js/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
 import '../styles/index.css'
-import GameManagement from './pages/gameManagement/GameManagement.jsx'
+import { App } from './app'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GameManagement/>
+    <BrowserRouter>
+      <App></App>
+    </BrowserRouter>    
   </StrictMode>,
 )
