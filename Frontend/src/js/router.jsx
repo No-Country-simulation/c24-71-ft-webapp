1d5e191558a2e53dd4f07fc9f8a25661bd76eab5
William <william_7527@hotmail.com>
2025-03-06 18:14:37 -0500

    refactor(router): Rename GameSessionJoin and LayoutGame components

--- Frontend/src/js/router.jsx
+++ Frontend/src/js/router.jsx
@@ -5,17 +5,17 @@ import { Navigate, Route, Routes } from "react-router"
 
 import LayoutAuth from "./component/layout/LayoutAuth"
 import LayoutDashboard from "./component/layout/LayoutDashboard"
-import LayoutPlay from "./component/layout/LayoutPlay"
+import LayoutGame from "./component/layout/LayoutGame"
 import PrivateRoute from "./component/layout/PrivateRoute"
 import PublicRoute from "./component/layout/PublicRoute"
 
 import { Home } from "./pages/home/Home"
 import { Login } from "./pages/auth/login"
 import { PatientManagement } from "./pages/patientManagement/patientManagement"
-import { PlayGame } from "./pages/play/PlayGame"
 import { Register } from "./pages/auth/Register"
 import GameManagement from "./pages/gameManagement/gameManagement"
 import NotFound from "./pages/noFound/NotFound"
+import GameSessionJoin from "./pages/gameSessionJoin/GameSessionJoin"
 
 
 export const AppRouter = () => {
@@ -33,8 +33,8 @@ export const AppRouter = () => {
 
     {/* Rutas de juego (públicas) */}
     <Route path="game-sessions/patient/join/:sessionId" element={<PublicRoute />}>
-        <Route element={<LayoutPlay />}>
-            <Route index element={<PlayGame />} />           
+        <Route element={<LayoutGame />}>
+            <Route index element={<GameSessionJoin />} />           
             <Route path="*" element={<NotFound />} />
       </Route>
     </Route>
