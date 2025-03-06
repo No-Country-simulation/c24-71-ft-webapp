a2a2803ddbf03f0aecdef6fec8b41f025b81118b
romina gonzalez <rominagonzalez.design@gmail.com>
2025-03-06 20:15:13 -0300

    fix(Patient): Fix responsive design of Patient's page

--- Frontend/src/js/pages/patientManagement/Patients.jsx
+++ Frontend/src/js/pages/patientManagement/Patients.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { MdEdit } from "react-icons/md";
 
 const Patients = () => {
   return (
-    <div className='grid grid-cols-[488px_1300px_150px] w-full'>
-      <div className='bg-[#F7F7F7] w-[1253px] h-[44px] border-solid border-1 border-[#939191] content-center'>
-        <ul className='text-[#4E5C82] text-xl font-normal flex justify-between px-10'>
-          <li>Columna1</li>
-          <li>Columna2</li>
-          <li>Columna3</li>
-          <li>Columna4</li>
-          <li>Columna5</li>
+    <div className='w-full'>
+      <div className='bg-[#F7F7F7] h-[44px] border-solid border-1 border-[#939191] flex items-center'>
+        <ul className='grid grid-cols-[1fr_6fr_6fr_1fr_1fr] gap-4 text-start text-[#4E5C82] text-xl font-normal px-10'>
+          <li>12.345.678</li>
+          <li>Soledad Vazquez</li>
+          <li>#00</li>
+          <li>soledad@gmail.com</li>
+          <li className='flex justify-end'><MdEdit color='#2D3147'/></li>
         </ul>
       </div>
     </div>
--- Frontend/src/js/pages/patientManagement/SearchPatient.jsx
+++ Frontend/src/js/pages/patientManagement/SearchPatient.jsx
@@ -4,7 +4,7 @@ import AddPatient from './AddPatient';
 
 const SearchPatient = () => {
   return (
-    <div className='text-black grid grid-cols-[156px_1005px_80px_28px_150px_150px] grid-rows-[141px_1fr_1fr] w-full max-w-none'>
+    <div className='text-black grid grid-cols-[0.32fr_2fr_0.15fr_0.1fr_0.7fr_0fr] grid-rows-[2.2fr_1fr_1fr] w-full'>
 			<input type='search' placeholder='Buscar...' className='bg-[#EEEEEE] w-full h-[67px] col-start-2 row-start-2 col-span-1 border-solid border-2 border-[#A69487] shadow-[0_4px_4px_rgba(0,0,0,0.25)] px-4' />
 			<button className='bg-[#EEEEEE] w-full h-[67px] col-start-3 row-start-2 col-span-1 border-solid border-2 border-[#A69487] mr-1 shadow-[0_4px_4px_rgba(0,0,0,0.25)] flex justify-center items-center'><MdSearch size='23px' /></button>
       <div className='col-start-5 row-start-2'>
@@ -15,4 +15,3 @@ const SearchPatient = () => {
 }
 
 export default SearchPatient;
-//absolute top-[141px] left-[1501px] button
