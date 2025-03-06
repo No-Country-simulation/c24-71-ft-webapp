e8c19dbd8807bba36485389f6f5d1981230905cc
romina gonzalez <rominagonzalez.design@gmail.com>
2025-03-06 12:47:44 -0300

    fix(Patient): Fix margin left error

--- Frontend/src/js/pages/patientManagement/ListPatients.jsx
+++ Frontend/src/js/pages/patientManagement/ListPatients.jsx
@@ -3,7 +3,7 @@ import Patients from './Patients';
 
 const ListPatients = () => {
   return (
-    <div className='grid grid-cols-[488px_1300px_150px] grid-rows-[1fr_30px_1fr] w-full'>
+    <div className='grid grid-cols-[156px_1300px_150px] grid-rows-[1fr_30px_1fr] w-full'>
 				<div className='bar bg-[#EEEEEE] w-full max-w-[1253px] h-[68px] col-start-2 border-solid border-1 border-[#939191] mb-10 content-center'>
           <ul className='text-[#3F4B6E] text-xl font-semibold flex justify-between px-10'>
             <li>DNI</li>
--- Frontend/src/js/pages/patientManagement/SearchPatient.jsx
+++ Frontend/src/js/pages/patientManagement/SearchPatient.jsx
@@ -4,7 +4,7 @@ import AddPatient from './AddPatient';
 
 const SearchPatient = () => {
   return (
-    <div className='text-black grid grid-cols-[488px_1005px_80px_28px_150px_150px] grid-rows-[141px_1fr_1fr] w-full max-w-none'>
+    <div className='text-black grid grid-cols-[156px_1005px_80px_28px_150px_150px] grid-rows-[141px_1fr_1fr] w-full max-w-none'>
 			<input type='search' placeholder='Buscar...' className='bg-[#EEEEEE] w-full h-[67px] col-start-2 row-start-2 col-span-1 border-solid border-2 border-[#A69487] shadow-[0_4px_4px_rgba(0,0,0,0.25)] px-4' />
 			<button className='bg-[#EEEEEE] w-full h-[67px] col-start-3 row-start-2 col-span-1 border-solid border-2 border-[#A69487] mr-1 shadow-[0_4px_4px_rgba(0,0,0,0.25)] flex justify-center items-center'><MdSearch size='23px' /></button>
       <div className='col-start-5 row-start-2'>
