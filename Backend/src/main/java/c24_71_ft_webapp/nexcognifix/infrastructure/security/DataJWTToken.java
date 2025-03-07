11a219f5f47a2dc7722488c9d33f9951f05c0572
ChayChaio <chaychaio@gmail.com>
2025-03-06 22:52:55 -0300

    refactor(login) add professionals name to response

--- Backend/src/main/java/c24_71_ft_webapp/nexcognifix/infrastructure/security/DataJWTToken.java
+++ Backend/src/main/java/c24_71_ft_webapp/nexcognifix/infrastructure/security/DataJWTToken.java
@@ -1,4 +1,9 @@
 package c24_71_ft_webapp.nexcognifix.infrastructure.security;
 
-public record DataJWTToken(String JWTToken) {
+public record DataJWTToken
+        (
+        String JWTToken,
+        String professionalName
+        )
+{
 }
