07483cfd27c8884fcb51425a4e3e05d732de9ea1
William <william_7527@hotmail.com>
2025-03-08 11:13:54 -0500

    fix(components): Corrected import path for App component

--- Frontend/src/js/main.jsx
+++ Frontend/src/js/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import '../styles/index.css'
-import { App } from './app'
+import { App } from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
