a6587113196de987dcd93ab66ee441c81691b898
orwen97 <rommirose97@gmail.com>
2025-03-11 13:02:40 -0300

    chore(Patient): update github email

--- Frontend/src/js/pages/patientManagement/AddPatient.jsx
+++ Frontend/src/js/pages/patientManagement/AddPatient.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { useState } from 'react';
-import { MdPersonAdd } from 'react-icons/md';
-import { FaRegWindowClose } from 'react-icons/fa';
+import React from "react";
+import { useState } from "react";
+import { MdPersonAdd } from "react-icons/md";
+import { FaRegWindowClose } from "react-icons/fa";
 
 const AddPatient = () => {
     const [openPopUp, setOpenPopUp] = useState(false);
 
     const [values, setValues] = useState({
         //Save form data
-        name: '',
-        dni: '',
-        age: '',
-        email: '',
-        diagnosis: '',
+        name: "",
+        dni: "",
+        age: "",
+        email: "",
+        diagnosis: "",
     });
 
     const handleOnChange = (event) => {
@@ -31,95 +31,107 @@ const AddPatient = () => {
     };
 
     return (
-        <div className='grid grid-cols-[0.7fr_0.8fr]'>
+        <div className="grid grid-cols-[0.7fr_0.8fr]">
             <button
                 onClick={() => setOpenPopUp(true)}
-                className='bg-[#4E5C82] w-full h-[69px] rounded-[8px] flex justify-center items-center hover:bg-[#65749e] transform active:translate-y-[2px] transition-all duration-300'
+                className="bg-[#4E5C82] w-full h-[69px] rounded-[8px] flex justify-center items-center hover:bg-[#65749e] transform active:translate-y-[2px] transition-all duration-300"
             >
-                <MdPersonAdd size='33px' color='#ECE5DE' />
+                <MdPersonAdd size="33px" color="#ECE5DE" />
             </button>
 
             {/* pop up Add Patient */}
             {openPopUp && (
-                <div className='grid fixed top-1/2 left-1/2 transform -translate-x-1/3 -translate-y-1/2 w-full max-w-[750px] h-[560px] bg-[#4E5C82] text-white p-8 z-50'>
-                    <div className='flex justify-between'>
-                        <h2 className='text-2xl pb-10 pt-4 mx-auto'>Agregar Paciente</h2>
+                <div className="grid fixed top-1/2 left-1/2 transform -translate-x-1/3 -translate-y-1/2 w-full max-w-[750px] h-[560px] bg-[#4E5C82] text-white rounded-xl p-8 z-50">
+                    <div className="flex justify-between">
+                        <h2 className="text-2xl pb-10 pt-4 mx-auto">
+                            Agregar Paciente
+                        </h2>
                         <button
                             onClick={() => setOpenPopUp(false)}
-                            className='self-start'>
+                            className="self-start"
+                        >
                             <FaRegWindowClose size={24} />
                         </button>
                     </div>
                     <form
                         onSubmit={handleSubmit}
-                        className='grid grid-cols-[1.2fr_1.3fr_0.5fr_1.3fr] grid-rows-[0.5fr_0.5fr_1fr_0.5fr] gap-4'>
-                        <div className='grid col-span-2 content-center'>
-                            <label htmlFor='name'>Nombre y Apellido:</label>
+                        className="grid grid-cols-[1.2fr_1.3fr_0.5fr_1.3fr] grid-rows-[0.5fr_0.5fr_1fr_0.5fr] gap-4"
+                    >
+                        <div className="grid col-span-2 content-center">
+                            <label htmlFor="name">Nombre y Apellido:</label>
                             <input
-                              type='text'
-                              name='name'
-                              value={values.name}
-                              placeholder='José Peréz'
-                              maxLength='20'
-                              required
-                              onChange={handleOnChange}
-                              className='bg-[#EEEEEE] col-start-2 max-w-[200px] h-[40px] p-2 placeholder-[#939191] text-[#616060]'
+                                type="text"
+                                name="name"
+                                value={values.name}
+                                placeholder="José Peréz"
+                                maxLength="20"
+                                required
+                                onChange={handleOnChange}
+                                className="bg-[#EEEEEE] col-start-2 max-w-[200px] h-[40px] p-2 placeholder-[#939191] text-[#616060]"
                             />
                         </div>
-                        <div className='grid col-span-2 content-center'>
-                            <label htmlFor='dni' className=''>DNI:</label>
+                        <div className="grid col-span-2 content-center">
+                            <label htmlFor="dni" className="">
+                                DNI:
+                            </label>
                             <input
-                              type='number'
-                              name='dni'
-                              value={values.dni}
-                              placeholder='11.111.111'
-                              required
-                              onChange={handleOnChange}
-                              className='bg-[#EEEEEE] col-start-4 max-w-[200px] h-[40px] p-2 placeholder-[#939191] text-[#616060]'
+                                type="number"
+                                name="dni"
+                                value={values.dni}
+                                placeholder="11.111.111"
+                                required
+                                onChange={handleOnChange}
+                                className="bg-[#EEEEEE] col-start-4 max-w-[200px] h-[40px] p-2 placeholder-[#939191] text-[#616060]"
                             />
                         </div>
-                        <div className='grid col-span-2 content-center'>
-                            <label htmlFor='age' className=''>Edad:</label>
+                        <div className="grid col-span-2 content-center">
+                            <label htmlFor="age" className="">
+                                Edad:
+                            </label>
                             <input
-                              type='number'
-                              name='age'
-                              value={values.age}
-                              placeholder='99'
-                              required
-                              min= '1'
-                              max='99'
-                              onChange={handleOnChange}
-                              className='bg-[#EEEEEE] col-start-2 max-w-[200px] h-[40px] ml-24 p-2 placeholder-[#939191] text-[#616060]'
+                                type="number"
+                                name="age"
+                                value={values.age}
+                                placeholder="99"
+                                required
+                                min="1"
+                                max="99"
+                                onChange={handleOnChange}
+                                className="bg-[#EEEEEE] col-start-2 max-w-[200px] h-[40px] p-2 placeholder-[#939191] text-[#616060]"
                             />
                         </div>
-                        <div className='grid col-span-2 content-center'>
-                            <label htmlFor='email' className=''>E-mail:</label>
+                        <div className="grid col-span-2 content-center">
+                            <label htmlFor="email" className="">
+                                E-mail:
+                            </label>
                             <input
-                              type='email'
-                              name='email'
-                              value={values.email}
-                              placeholder='joseperez@gmail.com'
-                              onChange={handleOnChange}
-                              className='bg-[#EEEEEE] col-start-4 max-w-[200px] h-[40px] p-2 placeholder-[#939191] text-[#616060]'
+                                type="email"
+                                name="email"
+                                value={values.email}
+                                placeholder="joseperez@gmail.com"
+                                onChange={handleOnChange}
+                                className="bg-[#EEEEEE] col-start-4 max-w-[200px] h-[40px] p-2 placeholder-[#939191] text-[#616060]"
                             />
                         </div>
-                        <div className='col-span-4'>
-                            <label htmlFor='diagnosis' className=''>Diagnóstico:</label>
+                        <div className="col-span-4">
+                            <label htmlFor="diagnosis" className="">
+                                Diagnóstico:
+                            </label>
                             <textarea
-                              type='text'
-                              name='diagnosis'
-                              value={values.diagnosis}
-                              placeholder='Escriba aquí'
-                              onChange={handleOnChange}
-                              className='bg-[#EEEEEE] w-full h-[100px] p-2 placeholder-[#939191] text-[#616060]'
+                                type="text"
+                                name="diagnosis"
+                                value={values.diagnosis}
+                                placeholder="Escriba aquí"
+                                onChange={handleOnChange}
+                                className="bg-[#EEEEEE] w-full h-[100px] mt-2 p-2 placeholder-[#939191] text-[#616060]"
                             />
                         </div>
-                        <div className='col-span-4 justify-self-center content-center'>
+                        <div className="col-span-4 justify-self-center content-center">
                             <button
-                              typeof='submit'
-                              className='bg-[#4E5C82] p-2 border border-[#ECE5DE] rounded-[8px] hover:bg-[#65749e] transform active:translate-y-[2px] transition-all duration-300'
+                                typeof="submit"
+                                className="bg-[#4E5C82] p-2 border border-[#ECE5DE] rounded-[8px] hover:bg-[#65749e] transform active:translate-y-[2px] transition-all duration-300"
                             >
-                              Agregar
+                                Agregar
                             </button>
                         </div>
                     </form>
