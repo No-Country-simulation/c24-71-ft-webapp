63b3e8d65351fac86919ce3b2283a3bc9edc45d1
orwen97 <rommirose97@gmail.com>
2025-03-12 14:05:27 -0300

    Feat(Patient): Get patients data from api

--- Frontend/src/js/pages/patientManagement/Patients.jsx
+++ Frontend/src/js/pages/patientManagement/Patients.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { MdEdit } from "react-icons/md";
+import api from '../../api/axiosConfig.js';
 
 const Patients = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true)
+  //Catch Patients data from api
+  const patientsData = async () => {
+    try{
+      const response = await api.get('/api/patients');
+      console.log("Respuesta de la API:", response);
+      setData(response.data);
+    } catch (error){
+      console.error("Error al obtener datos:", error)
+      setError(error.response?.data?.error ?? "Hubo un error al cargar la lista de pacientes.");
+    } finally {
+    setLoading(false);
+  }
+  };
+  useEffect(() => {
+    patientsData();
+  }, [])
 
-  const patientsData = [
-    {dni: "12.345.678", name: "Mariana Vazquez", age: "65", email: "mariana@gmail.com" },
-    {dni: "12.345.678", name: "Mariana Vazquez", age: "65", email: "mariana@gmail.com" },
-    {dni: "12.345.678", name: "Mariana Vazquez", age: "65", email: "mariana@gmail.com" },
-    {dni: "12.345.678", name: "Mariana Vazquez", age: "65", email: "mariana@gmail.com" },
-    {dni: "12.345.678", name: "Mariana Vazquez", age: "65", email: "mariana@gmail.com" },
-    {dni: "12.345.678", name: "Mariana Vazquez", age: "65", email: "mariana@gmail.com" },
-    {dni: "12.345.678", name: "Mariana Vazquez", age: "65", email: "mariana@gmail.com" },
-    {dni: "12.345.678", name: "Mariana Vazquez", age: "65", email: "mariana@gmail.com" },
-    {dni: "12.345.678", name: "Mariana Vazquez", age: "65", email: "mariana@gmail.com" },
-    {dni: "12.345.678", name: "Mariana Vazquez", age: "65", email: "mariana@gmail.com" }
-  ]
+  console.log(data);
 
   return (
     <div className='w-full '>
-      {patientsData.map((patient, index) => (
+      {/* {patientsData.map((patient, index) => (
       <div key={index} className={`bg-[#F7F7F7] h-[44px] border-solid border-[#939191] ${index === 0 ? "border-t-0" : "border-t"} content-center`}>
         <ul className='grid grid-cols-[1fr_1.5fr_0.5fr_1.5fr_0.2fr] text-start text-[#4E5C82] text-xl font-normal px-10'>
           <li>{patient.dni}</li>
@@ -29,7 +40,7 @@ const Patients = () => {
         </ul>
       </div>
       )
-      )}
+      )} */}
     </div>
   )
 }
