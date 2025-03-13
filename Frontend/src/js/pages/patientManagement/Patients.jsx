fe1091973c067ad8445210efab7a32403445d816
orwen97 <rommirose97@gmail.com>
2025-03-13 14:39:22 -0300

    refacture(Patient): Code enhancement in Pacientes page

--- Frontend/src/js/pages/patientManagement/Patients.jsx
+++ Frontend/src/js/pages/patientManagement/Patients.jsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 import { MdEdit } from "react-icons/md";
 import api from '../../api/axiosConfig.js';
 import { FaRegClipboard } from "react-icons/fa";
+import { MdSearch } from "react-icons/md";
+import AddPatient from './AddPatient';
 
 const Patients = () => {
   const [data, setData] = useState([]);
@@ -27,47 +29,45 @@ const Patients = () => {
   console.log(data);
 
   return (
-    <div className='w-full '>
-      {data.map((patient, index) => (
-      <div key={index} className={`bg-[#F7F7F7] h-[44px] border-solid border-[#939191] ${index === 0 ? "border-t-0" : "border-t"} content-center`}>
-        <ul className='grid grid-cols-[1fr_1.2fr_0.45fr_1.6fr_0.2fr] text-start text-[#4E5C82] text-xl font-normal px-10'>
-          <li>{patient.dni}</li>
-          <li className=''>{patient.name}</li>
-          <li>50</li>
-          <li className='pl-8'>{patient.email}</li>
-          <li className='flex justify-center items-center pr-4 gap-4'>
-            <MdEdit color='#2D3147'/>
-            <FaRegClipboard color='#2D3147'/>
-            </li>
-        </ul>
+    <div className='w-full'>
+      {/* Searcher */}
+    <div className='text-black grid grid-cols-[0.32fr_2fr_0.15fr_0.1fr_0.7fr_0fr] grid-rows-[1fr_1fr_1fr] w-full '>
+			<input type='search' placeholder='Buscar...' className='bg-[#EEEEEE] w-full h-[67px] col-start-2 row-start-2 col-span-1 border-solid first:border-r-0 border-1 border-[#A69487] shadow-[0_4px_4px_rgba(0,0,0,0.25)] px-4 focus:outline-none' />
+			<button className='bg-[#EEEEEE] w-full h-[67px] col-start-3 row-start-2 col-span-1 border-solid border-1 border-[#A69487] last:border-l-0 mr-1 shadow-[0_4px_4px_rgba(0,0,0,0.25)] cursor-pointer flex justify-center items-center hover:bg-[#e0e0e0] transform active:translate-y-[2px] transition-all duration-300'>
+        <MdSearch size='23px' />
+      </button>
+      <div className='col-start-5 row-start-2'>
+        <AddPatient /> {/*Add patient button / popup */}
+    </div>
+    </div>
+    {/* Show Patients table */}
+      <div className='grid grid-cols-[0.17fr_1.4fr_0.2fr]'>
+      <table className='w-full col-start-2 border border-[#939191] border-collapse'>
+        <thead className='bg-[#EEEEEE] border-b-4 border-gray-300 text-[#3F4B6E] text-xl'>
+          <th className='p-3 text-left font-semibold'>DNI</th>
+          <th className='p-3 text-left font-semibold'>Nombre y Apellido</th>
+          <th className='p-3 text-left font-semibold'>Edad</th>
+          <th className='p-3 text-left font-semibold'>Email</th>
+          <th className='p-3 text-left font-semibold'>Acciones</th>
+        </thead>
+        {data.map((patient, index) => (
+        <tbody key={index} className={`bg-[#f0f0f0] h-[44px]  border-solid border-[#939191] ${index === 0 ? "border-t-0" : "border-t"} content-center`}>
+          <tr className='text-[#4E5C82] text-xl'>
+            <th className='p-3 text-left font-normal'>{patient.dni}</th>
+            <th className='p-3 text-left font-normal'>{patient.name}</th>
+            <th className='p-3 text-left font-normal'>50</th>
+            <th className='p-3 text-left font-normal'>{patient.email}</th>
+            <th className='p-3 flex gap-4 justify-start pl-6'>
+            <MdEdit className='cursor-pointer' color='#2D3147'/>
+            <FaRegClipboard className='cursor-pointer'  color='#2D3147'/>
+            </th>
+          </tr>
+        </tbody>
+          )
+          )}
+      </table>
       </div>
-      )
-      )}
     </div>
   )
 }
 export default Patients;
-
-
-
-
-
-
-
-// const Patients = () => {
-//   return (
-//     <div className='w-full'>
-//       <div className='bg-[#F7F7F7] h-[44px] border-t border-solid border-[#939191] content-center'>
-//         <ul className='grid grid-cols-[1fr_1.5fr_0.5fr_1.5fr_0.2fr] text-start text-[#4E5C82] text-xl font-normal px-10'>
-//           <li>12.345.678</li>
-//           <li>Soledad Vazquez</li>
-//           <li className='text-start'>#00</li>
-//           <li>soledad@gmail.com</li>
-//           <li className='flex justify-center pr-4 items-center'><MdEdit color='#2D3147'/></li>
-//         </ul>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Patients;
